9decbfea1ce18fe0ba2d46ceaed7da1ed740dd0f
Pawan Sharma <pawansharma93520@gmail.com>
2022-12-01 17:54:18 +0530

    adding flags

---
+++ cli/cli.go
@@ -0,0 +1,11 @@
+package cli
+
+type RequestBody struct {
+	SourceLang string
+	TargetLang string
+	SourceText string
+}
+
+func RequestTranslate(r *RequestBody) {
+
+}
--- main.go
+++ main.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
 
-func main(){
+	"github.com/vikashparashar/golang_project_03/cli"
+)
 
+var (
+	sourceLang string
+	targetLang string
+	sourceText string
+)
+
+func init() {
+	flag.StringVar(&sourceLang, "s", "en", "Convert From [english]")
+	flag.StringVar(&targetLang, "t", "fr", "Convert Into [french]")
+	flag.StringVar(&sourceText, "st", "", "Text That Need To Be Converted Into Target Language")
+
+}
+
+func main() {
+	flag.Parse()
+
+	// is user dones not enter any flag , then show them options available for flags
+	// and exit the program immediatly
+	if flag.NArg() == 0 {
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	var reqBody = &cli.RequestBody{
+		SourceLang: sourceLang,
+		TargetLang: targetLang,
+		SourceText: sourceText,
+	}
+
+	cli.RequestTranslate(reqBody)
 }
